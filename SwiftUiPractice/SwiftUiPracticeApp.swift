eadf7cdc607701efd26c21599e86ec3122ce0e85
eyvonne <eyvonne@users.noreply.github.com>
2020-07-08 18:04:50 -0700

    Initial Commit

---
+++ SwiftUiPractice/SwiftUiPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiPracticeApp.swift
+//  SwiftUiPractice
+//
+//  Created by Eyvonne Geordan on 7/8/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
